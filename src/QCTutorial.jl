de88ffb504aeb80d287d756218c5a9d4dd0f5c3b
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2023-11-14 18:15:05 -0500

    Update notebooks

--- src/QCTutorial.jl
+++ src/QCTutorial.jl
@@ -2,33 +2,101 @@ module QCTutorial
 
 using LinearAlgebra
 
-export ⊗
-export State, Gate, H, X, I
+export ⊗, ∧
+export shift
+export Ket, @ket_str
+export Bra, @bra_str
+export Gate, H, X, I
 
 const ⊗ = kron
 const ℂ = ComplexF64
 
-struct State
+const DISPLAY_PRECISION = 3
+
+abstract type State end
+
+struct Ket <: State
     vector::Vector{ℂ}
 end
 
-function State(v::Vector{T}) where {T<:Number}
+function Ket(v::AbstractVector{T}) where {T<:Number}
     @assert ispow2(length(v))
 
-    return State(convert(Vector{ℂ}, v))
+    return Ket(convert(Vector{ℂ}, v))
 end
 
-function State(k::Integer, n::Integer)
+struct Bra <: State
+    vector::Adjoint{ℂ,Vector{ℂ}}
+end
+
+function Bra(v::Adjoint{T,V}) where {T<:Number,V<:AbstractVector{T}}
+    @assert ispow2(length(v))
+
+    return Bra(adjoint(convert(Vector{ℂ}, v)))
+end
+
+vector(ψ::Ket) = ψ.vector
+vector(ψ::Bra) = ψ.vector
+
+adjoint(ψ::Ket) = Bra(vector(ψ)')
+adjoint(ψ::Bra) = Ket(vector(ψ)')
+
+function Base.length(ψ::State)
+    return length(vector(ψ))
+end
+
+function nbits(ψ::State)
+    return round(Int, log2(length(ψ)))
+end
+
+function Ket(k::Integer, n::Integer = 1)
     @assert 0 ≤ k < 2^n
 
     v = zeros(2^n)
+
     v[k + 1] = 1
 
-    return State(v)
+    return Ket(v)
 end
 
-function Base.show(io::IO, ψ::State)
-    n = round(Int, log2(length(ψ.vector)))
+function Bra(k::Integer, n::Integer = 1)
+    @assert 0 ≤ k < 2^n
+
+    v = zeros(2^n)
+
+    v[k + 1] = 1
+
+    return Bra(v)
+end
+
+function Ket(s::AbstractString)
+    n = length(s)
+    i = parse(Int, s; base = 2)
+
+    return Ket(i, n)
+end
+
+function Bra(s::AbstractString)
+    n = length(s)
+    i = parse(Int, s; base = 2)
+
+    return Bra(i, n)
+end
+
+macro ket_str(bits::AbstractString)
+    return quote
+        Ket($(esc(bits)))
+    end
+end
+
+macro bra_str(bits::AbstractString)
+    return quote
+        Bra($(esc(bits)))
+    end
+end
+
+function Base.show(io::IO, ψ::Ket)
+    n = nbits(ψ)
 
     basis = []
 
@@ -36,24 +104,56 @@ function Base.show(io::IO, ψ::State)
 
     for i = eachindex(ψ.vector)
         α = ψ.vector[i]
+        c = round(α; digits = DISPLAY_PRECISION)
 
-        if α != 0
-            if isreal(α)
-                c = real(α)
+        if !iszero(c)
+            s = if isreal(c)
+                if isone(real(c))
+                    ""
+                else
+                    "$(real(c))"
+                end
+            elseif isimag(c)
+                "$(imag(c))im"
             end
 
-            if isimag(α)
-                c = "$(imag(α))im"
-            end
-
-            push!(basis, "$(c) |$(join(reverse!(digits(i - 1; base = 2, pad = n))))⟩")
+            push!(basis, "$(s) |$(join(reverse!(digits(i - 1; base = 2, pad = n))))⟩")
         end
     end
 
     return join(io, basis, " + ")
 end
 
-function Base.show(io::IO, ::MIME"text/latex", ψ::State)
+function Base.show(io::IO, ψ::Bra)
+    n = nbits(ψ)
+
+    basis = []
+
+    isimag(x) = !isreal(x) && iszero(real(x))
+
+    for i = eachindex(ψ.vector)
+        α = ψ.vector[i]
+        c = round(α; digits = DISPLAY_PRECISION)
+
+        if !iszero(c)
+            s = if isreal(c)
+                if isone(real(c))
+                    ""
+                else
+                    "$(real(c))"
+                end
+            elseif isimag(c)
+                "$(imag(c))im"
+            end
+
+            push!(basis, "$(s) ⟨$(join(reverse!(digits(i - 1; base = 2, pad = n))))|")
+        end
+    end
+
+    return join(io, basis, " + ")
+end
+
+function Base.show(io::IO, ::MIME"text/latex", ψ::Ket)
     n = round(Int, log2(length(ψ.vector)))
 
     basis = []
@@ -62,17 +162,49 @@ function Base.show(io::IO, ::MIME"text/latex", ψ::State)
 
     for i = eachindex(ψ.vector)
         α = ψ.vector[i]
+        c = round(α; digits = DISPLAY_PRECISION)
 
-        if α != 0
-            if isreal(α)
-                c = real(α)
+        if !iszero(c)
+            s = if isreal(c)
+                if isone(real(c))
+                    ""
+                else
+                    "$(real(c))"
+                end
+            elseif isimag(c)
+                "$(imag(c))im"
             end
 
-            if isimag(α)
-                c = "$(imag(α)) i"
+            push!(basis, "$(s) \\vert{}$(join(reverse!(digits(i - 1; base = 2, pad = n))))\\rangle{}")
+        end
+    end
+
+    return print(io, "\$", join(basis, " + "), "\$")
+end
+
+function Base.show(io::IO, ::MIME"text/latex", ψ::Bra)
+    n = round(Int, log2(length(ψ.vector)))
+
+    basis = []
+
+    isimag(x) = !isreal(x) && iszero(real(x))
+
+    for i = eachindex(ψ.vector)
+        α = ψ.vector[i]
+        c = round(α; digits = DISPLAY_PRECISION)
+
+        if !iszero(c)
+            s = if isreal(c)
+                if isone(real(c))
+                    ""
+                else
+                    "$(real(c))"
+                end
+            elseif isimag(c)
+                "$(imag(c))im"
             end
 
-            push!(basis, "$(c) \\vert{}$(join(reverse!(digits(i - 1; base = 2, pad = n))))\\rangle{}")
+            push!(basis, "$(s) \\langle{}$(join(reverse!(digits(i - 1; base = 2, pad = n))))\\vert{}")
         end
     end
 
@@ -90,24 +222,34 @@ function Gate(M::Matrix{T}) where {T<:Number}
     return Gate(convert(Matrix{ℂ}, M))
 end
 
+const _I = Gate([1 0; 0 1])
 const _H = Gate((1 / √2) * [1 1; 1 -1])
 const _X = Gate([0 1; 1 0])
-const _I = Gate([1 0; 0 1])
 
-function H(n::Integer)
-    if n < 0
-        return inv(H(-n))
-    elseif n == 0
+function gate(k::Integer, U::Gate)
+    if k < 0
+        return inv(H(-k))
+    elseif k == 0
         return _I
-    elseif n == 1
-        return _H
-    else
-        return _H ⊗ H(n - 1)
+    elseif k == 1
+        return U
+    elseif k % 2 == 0
+        V = gate(k ÷ 2, U)
+
+        return V ⊗ V
+    else # k % 2 == 1
+        V = gate(k ÷ 2, U)
+
+        return V ⊗ V ⊗ U
     end
 end
 
-function Base.kron(ψ::State, ϕ::State)
-    return State(kron(ψ.vector, ϕ.vector))
+I(n::Integer) = gate(n, _I)
+H(n::Integer) = gate(n, _H)
+X(n::Integer) = gate(n, _X)
+
+function Base.kron(ψ::S, ϕ::S) where {S<:State}
+    return S(kron(vector(ψ), vector(ϕ)))
 end
 
 function Base.kron(U::Gate, V::Gate)
@@ -122,36 +264,88 @@ function Base.:(*)(c::Number, U::Gate)
     return Gate(U.matrix * c)
 end
 
-function Base.:(*)(ψ::State, c::Number)
-    return State(ψ.vector * c)
+function Base.:(*)(ψ::S, c::Number) where {S<:State}
+    return S(ψ.vector * c)
 end
 
-function Base.:(*)(c::Number, ψ::State)
-    return State(ψ.vector * c)
+function Base.:(*)(c::Number, ψ::S) where {S<:State}
+    return S(ψ.vector * c)
 end
 
-function Base.:(*)(U::Gate, ψ::State)
-    return State(U.matrix * ψ.vector)
+function Base.:(*)(ϕ::Bra, ψ::Ket)
+    return vector(ϕ) * vector(ψ)
+end
+
+function Base.:(*)(U::Gate, ψ::Ket)
+    return Ket(U.matrix * vector(ψ))
+end
+
+function Base.:(*)(ψ::Bra, U::Gate)
+    return Bra(vector(ψ) * U.matrix)
 end
 
 function Base.:(*)(U::Gate, V::Gate)
     return Gate(U.matrix * V.matrix)
 end
 
-function Base.:(+)(ψ::State, ϕ::State)
-    return State(ψ.vector + ϕ.vector)
+function Base.:(+)(ψ::S, ϕ::S) where {S<:State}
+    return S(vector(ψ) + vector(ϕ))
 end
 
 function Base.:(+)(U::Gate, V::Gate)
     return Gate(U.matrix + V.matrix)
 end
 
-function Base.:(-)(ψ::State, ϕ::State)
-    return State(ψ.vector - ϕ.vector)
+function Base.:(-)(ψ::S, ϕ::S) where {S<:State}
+    return S(ψ.vector - ϕ.vector)
 end
 
 function Base.:(-)(U::Gate, V::Gate)
     return Gate(U.matrix - V.matrix)
+end
+
+function outer(ψ::Ket, ϕ::Bra)
+    @assert length(ψ) == length(ϕ) "Dimension mismatch"
+
+    return Gate(vector(ψ) * vector(ϕ))
+end
+
+function outer(ψ::S, c::Number) where {S<:State}
+    return S(vector(ψ) * c)
+end
+
+function outer(c::Number, ψ::S) where {S<:State}
+    return S(vector(ψ) * c)
+end
+
+const ∧ = outer
+
+# Shift Operator
+function shift(n::Integer)
+    σ₋ = ket"0" ∧ bra"0"
+    σ₊ = ket"1" ∧ bra"1"
+    
+    m = ceil(Int, log2(n))
+    
+    Δ₋ = zeros(ComplexF64, 2^m, 2^m)
+    Δ₊ = zeros(ComplexF64, 2^m, 2^m)
+
+
+    for i = 0:(n-1)
+        δᵢ = Bra(i, m) # ⟨i|
+        
+        if i > 0
+            δ₋ = Ket(i-1, m) # |i-1⟩
+            Δ₋ .+= (δ₋ ∧ δᵢ).matrix
+        end
+        
+        if i < n - 1
+            δ₊ = Ket(i+1, m) # |i+1⟩
+            Δ₊ .+= (δ₊ ∧ δᵢ).matrix
+        end
+    end
+        
+    return σ₋ ⊗ Gate(Δ₋) + σ₊ ⊗ Gate(Δ₊)
 end
 
 end # module QCTuorial
